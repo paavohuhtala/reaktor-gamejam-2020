ca1090487f94ce76c5611208e1d96599d664cfeb
Jere Meriluoto <jeremeriluoto@reaktor.com>
2020-02-29 13:36:14 +0200

    Add high score functionality

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,15 +11,30 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
-    public float timeLeft = 30.0f;
+    public float timeLeft = 5.0f;
     public int score = 0;
     public Text startText; // used for showing countdown from 3, 2, 1 
     public Text scoreText;
+    public Text playAgainText;
+    public Text hightScoreText;
 
     public GameState state;
 
+    private void Start()
+    {
+        hightScoreText.text = getHighScore().ToString();
+    }
+
     void Update()
     {
+        if(state == GameState.Gameover && Input.GetKey(KeyCode.Return))
+        {
+            if (score > getHighScore())
+                setHighScore(score);
+            
+            SceneManager.LoadScene("Main");
+        }
+
         if(timeLeft > 0) 
         {
             timeLeft -= Time.deltaTime;
@@ -29,11 +44,26 @@ public class GameManager : MonoBehaviour
         else
         {
             startText.text = "Game Over!";
+            playAgainText.text = "Press Enter to play again!";
             state = GameState.Gameover;
         }
     }
 
     public void AddScore(int newScore) {
         score += newScore;
+
+        if (score > getHighScore())
+            setHighScore(score);
+    }
+
+    private int getHighScore()
+    {
+        return PlayerPrefs.GetInt("High Score");
+    }
+
+    private void setHighScore(int newHighScore)
+    {
+        PlayerPrefs.SetInt("High Score", newHighScore);
+        hightScoreText.text = getHighScore().ToString();
     }
 }
