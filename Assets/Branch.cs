6d962f02295f373b10663d2c74b9826583a68a98
paavohuhtala <paavo.huhtala@gmail.com>
2020-02-29 10:09:20 +0200

    Add branch component, make bird a prefab

---
+++ Assets/Branch.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Branch : MonoBehaviour
+{
+    public float StartingLength = 0.0f;
+    public float CurrentLength;
+    public float TargetLength = 5.0f;
+
+    public float StartingThickness = 1.0f;
+    public float TargetThickness = 2.0f;
+
+    public float GrowthRate = 1.0f;
+
+    public bool FullyGrown;
+
+    public GameObject BranchPrefab;
+
+    public Transform LeftBranchRoot; 
+    public Transform RightBranchRoot;
+
+    public Branch LeftBranch;
+    public Branch RightBranch;
+
+    void Start()
+    {
+        CurrentLength = StartingLength;
+    }
+
+    void Update()
+    {
+        if (CurrentLength < TargetLength)
+        {
+            CurrentLength = Mathf.Min(TargetLength, CurrentLength + GrowthRate * Time.deltaTime);
+
+            var growthLevel = CurrentLength / TargetLength;
+            var thickness = TargetThickness * growthLevel;
+            transform.localScale = new Vector3(thickness, CurrentLength, thickness);
+        }
+        else if (CurrentLength >= TargetLength && !FullyGrown)
+        {
+            FullyGrown = true;
+
+            var xAxis = new Vector3(1.0f, 0.0f, 0.0f);
+            var yOffset = new Vector3(0, -0.5f, 0.0f);
+
+            var leftBranch = Instantiate(BranchPrefab, LeftBranchRoot.transform.position + yOffset, Quaternion.AngleAxis(30f, xAxis));
+            var rightBranch = Instantiate(BranchPrefab, RightBranchRoot.transform.position + yOffset, Quaternion.AngleAxis(-30f, xAxis));
+        }
+    }
+}
