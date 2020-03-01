40773c7ad3cd2fad04b5483a62c04ab52c0f9d3b
paavohuhtala <paavo.huhtala@gmail.com>
2020-03-01 12:37:21 +0200

    Add music system

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine.UI;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
@@ -17,9 +16,9 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI playAgainText;
     public TextMeshProUGUI hightScoreText;
 
-    public float lengthOfDay = 10.0f;
+    public float lengthOfDay = 60.0f;
     public float currentTimeOfDay = 0.0f;
-
+    public float MusicStartsSpeedingUpAfter = 30.0f;
 
     public AudioSource worldRecordAudio;
     private bool worldRecordSaid;
@@ -28,24 +27,49 @@ public class GameManager : MonoBehaviour
 
     private string highScoreVariable = "High Score";
 
+    public AudioClip MusicStart;
+    public AudioClip MusicLoop;
+    public AudioClip MusicEnd;
+
+    public AudioSource MusicPlayer;
+
+    public float MusicSpeedupFactor = 0.5f;
+
     private void Start()
     {
         if(state == GameState.Running)
             hightScoreText.text = getHighScore().ToString();
+
+        PlayerPrefs.DeleteAll();
+        hightScoreText.text = getHighScore().ToString();
+
+        MusicPlayer.clip = MusicStart;
+        MusicPlayer.Play();
     }
 
     void Update()
     {
-        if(state == GameState.Starting)
+
+        if (state == GameState.Starting)
         {
-            if (Input.GetKey(KeyCode.Return))
             {
-                state = GameState.Running;
-                SceneManager.LoadScene("Main");
+                if (Input.GetKey(KeyCode.Return))
+                {
+                    state = GameState.Running;
+                    SceneManager.LoadScene("Main");
+                }
             }
+
+            return;
+        }
+
+        if (state == GameState.Gameover && Input.GetKey(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Main");
         }
         else if (state == GameState.Running)
         {
+            UpdateMusic();
             currentTimeOfDay = Time.timeSinceLevelLoad;
 
             if (state == GameState.Gameover && Input.GetKey(KeyCode.Return))
@@ -57,15 +81,22 @@ public class GameManager : MonoBehaviour
             {
                 scoreText.text = (score).ToString("0");
             }
-            else
-            {
-                playAgainText.text = "Press Enter to play again!";
-                state = GameState.Gameover;
-            }
+        }
+
+        if (currentTimeOfDay >= lengthOfDay && state == GameState.Running)
+        {
+            state = GameState.Gameover;
+            playAgainText.text = "Press Enter to play again!";
+            MusicPlayer.Stop();
+            MusicPlayer.clip = MusicEnd;
+            MusicPlayer.loop = false;
+            MusicPlayer.pitch = 1;
+            MusicPlayer.Play();
         }
     }
 
-    public void AddScore(int newScore) {
+    public void AddScore(int newScore)
+    {
         if (state != GameState.Running)
         {
             return;
@@ -95,5 +126,23 @@ public class GameManager : MonoBehaviour
     {
         PlayerPrefs.SetInt(highScoreVariable, newHighScore);
         hightScoreText.text = getHighScore().ToString();
+    }
+
+    public void UpdateMusic()
+    {
+        // Start music if not playing
+        if (state == GameState.Running && !MusicPlayer.isPlaying)
+        {
+            MusicPlayer.clip = MusicLoop;
+            MusicPlayer.loop = true;
+            MusicPlayer.Play();
+        }
+
+        var timeRemaining = lengthOfDay - currentTimeOfDay;
+
+        if (timeRemaining <= MusicStartsSpeedingUpAfter)
+        {
+            MusicPlayer.pitch = 1.0f + (1 - (timeRemaining / MusicStartsSpeedingUpAfter)) * MusicSpeedupFactor;
+        }
     }
 }
